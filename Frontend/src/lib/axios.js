1599a31b616d3d3c2913e9cb3264ff7b2502ba1e
meenusali <meenusali98@gmail.com>
2025-06-04 23:05:50 +0530

    url changes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+const protocol = window.location.protocol;
 
+console.log("protocol ",protocol)
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? `${protocol}//localhost:8080/api/v1`
+      : `${protocol}//157.245.254.133:5000/api/v1`,
   withCredentials: true,
 });
