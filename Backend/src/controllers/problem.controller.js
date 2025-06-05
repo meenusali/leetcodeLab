64460ec45a18e233ce53ee353ec2ed66f64e283c
meenusali <meenusali98@gmail.com>
2025-06-05 16:30:19 +0530

    changes

--- Backend/src/controllers/problem.controller.js
+++ Backend/src/controllers/problem.controller.js
@@ -51,6 +51,7 @@ export const createProblem = async (req, res) => {
 
       for (let i = 0; i < results.length; i++) {
         const result = results[i];
+        console.log("result",result)
         if (result.status.id !== 3) {
           return res.status(400).json({
             error: `Testcase ${i + 1} failed for language ${language}`,
